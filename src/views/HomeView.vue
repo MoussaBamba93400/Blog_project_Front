29f0b4ec2d82d58adb2f79ff060ab02ea84c3b66
Moussa bamba <moussabulls1@gmail.com>
2023-01-12 17:12:14 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <button class="btn">Button</button>
+  
+  </main>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="login">
+    <h1 class="border-2 border-black">Login</h1>
+  </div>
+</template>
+
